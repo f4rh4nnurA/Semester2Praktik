9e6dbb068614cb8e4b63ae848473179610407713
f4rh4nnurA <farhannuralifian1@gmail.com>
2024-06-06 11:05:07 +0700

    collection

---
+++ Minggu16/Collection/nomor5perc.java
@@ -0,0 +1,44 @@
+package Minggu16.Collection;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class nomor5perc {
+    public static void main(String[] args) {
+        Stack<String> fruits = new Stack<>();
+        fruits.push("Banana");
+        fruits.add("Orange");
+        fruits.add("Watermelon");
+        fruits.add("Leci");
+        fruits.push("Salak");
+
+        for (String fruit : fruits) {
+            System.out.printf("%s ", fruit);
+        }
+
+        System.out.println("\n" + fruits.toString());
+
+        while (!fruits.empty()) {
+            System.out.printf("%s ", fruits.pop());
+        }
+
+        fruits.push("Melon");
+        fruits.push("Strawberry");
+        fruits.push("Mango");
+        fruits.push("guava");
+        fruits.push("avocado");
+        System.out.println("");
+        for (Iterator<String> it = fruits.iterator(); it.hasNext();) {
+            String fruit = it.next();
+            System.out.printf("%s ", fruit);
+        }
+        System.out.println("");
+        fruits.stream().forEach(e -> {
+            System.out.printf("%s ", e);
+        });
+        System.out.println("");
+        for (int i = 0; i < fruits.size(); i++) {
+            System.out.printf("%s ", fruits.get(i));
+        }
+    }
+}
