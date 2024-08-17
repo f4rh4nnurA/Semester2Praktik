94b845f378dbdfbbf212f6ea9554913c1340c19b
f4rh4nnurA <farhannuralifian1@gmail.com>
2024-08-17 11:38:06 +0700

    update kode ascending bubble sort

--- Minggu6/Jobsheet6/daftarMahasiswaBerprestasi.java
+++ Minggu6/Jobsheet6/daftarMahasiswaBerprestasi.java
@@ -23,10 +23,10 @@ public class daftarMahasiswaBerprestasi {
     void bubbleSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
             for (int j = 0; j < listMhs.length - i - 1; j++) {
-                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                if (listMhs[j].ipk < listMhs[j + 1].ipk) {
                     Mahasiswa11 tmp = listMhs[j];
-                    listMhs[j] = listMhs[j - 1];
-                    listMhs[j - 1] = tmp;
+                    listMhs[j] = listMhs[j + 1];
+                    listMhs[j + 1] = tmp;
                 }
             }
         }
